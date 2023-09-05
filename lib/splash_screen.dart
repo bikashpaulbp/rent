1711cc1388fbc2e4bc9a606aa616646a9051c72b
bikashpaulbp <bikashpaulbp92@gmail.com>
2023-09-06 00:18:04 +0600

    gmail sign in

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:rent_management/screens/dashboard_page.dart';
+import 'package:rent_management/screens/google_signin.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -16,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
     Timer(const Duration(seconds: 5), () {
       Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => const Dashboard()));
+          context, MaterialPageRoute(builder: (context) => const SignInDemo()));
     });
   }
 
